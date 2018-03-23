646e729cccfc69ebc6be7f78356db608c236fb51
ラクッユミア ティアガラージ <cac32742@cac.co.jp>
2018-03-23 13:07:22 +0900

    IniatlVersion

---
+++ TestModelRun.m
@@ -0,0 +1,1 @@
+open_system('CI_Test');
---
+++ mat.bat
@@ -0,0 +1,7 @@
+echo off
+
+SET MATLABROOT="C:\Program Files\MATLAB\R2017a\bin"
+PATH=%MATLABROOT%;
+
+START matlab.exe -r C:\Users\cac32742\Desktop\test\TestModel\TestModelRun.m
+PAUSE
