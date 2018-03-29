dbb3a624fafaaf67b46aa6914d9a4ec22120b0cf
ラクッユミア ティアガラージ <cac32742@cac.co.jp>
2018-03-29 10:21:14 +0900

    updatedmscript

---
+++ TestModelRun.m
@@ -0,0 +1,7 @@
+
+model = 'CI_Test';
+load_system(model);
+open_system(model);
+sim(model);
+close_system(model);
+exit;
---
+++ mat.bat
@@ -0,0 +1,7 @@
+echo off
+
+SET MATLABROOT="C:\Program Files\MATLAB\R2018a\bin"
+PATH=%MATLABROOT%;
+
+matlab.exe  -nodisplay -nosplash -nodesktop -r "run TestModelRun" -logfile logfile exit;
+exit
