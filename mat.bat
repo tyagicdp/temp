615b00ffcfa4995b1e49a8a481abb0d89e6ec16d
Tyagaraj <37687862+tyagicdp@users.noreply.github.com>
2018-04-10 18:37:16 +0900

    Update mat.bat

--- mat.bat
+++ mat.bat
@@ -3,4 +3,4 @@ echo off
 SET MATLABROOT="C:\Program Files\MATLAB\R2018a\bin"
 PATH=%MATLABROOT%;
 
-matlab.exe -r "run TestModelRun" -logfile logfile exit;
+PATHmatlab.exe -r "run TestModelRun" -logfile logfile exit;
